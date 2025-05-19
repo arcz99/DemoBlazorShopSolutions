ca700e5b9c1888c4610ac38a9dd97374c5b96a7c
artur <artur.wojtasik007@gmail.com>
2025-05-20 00:32:10 +0200

    add-product/add-font/etc

--- PhoneShopShatedLibrary/Models/Product.cs
+++ PhoneShopShatedLibrary/Models/Product.cs
@@ -13,8 +13,9 @@ namespace PhoneShopShatedLibrary.Models
         public decimal? Price { get; set;}
         [Required, DisplayName("Product Image")]
         public string? Base64Img { get; set; }
+        [Required, Range(0, 99999)]
         public int Quantity { get; set; }
-        public bool Featured { get; set; }
+        public bool Featured { get; set; } = false;
         public DateTime DateUpLoaded { get; set; } = DateTime.Now;
     }
 }
--- ShopServer/Controllers/ProductController.cs
+++ ShopServer/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PhoneShopShatedLibrary.Contracts;
+using PhoneShopShatedLibrary.Models;
+using PhoneShopShatedLibrary.Responses;
 
 namespace ShopServer.Controllers
 {
@@ -15,5 +16,17 @@ namespace ShopServer.Controllers
             this.productService = productService;
         }
         [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAllProducts(bool featured)
+        {
+            var products = await productService.GetAllProducts(featured); return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse>> AddProduct(Product model)
+        {
+            if (model is null) return BadRequest("Model is Null");
+            var response = await productService.AddProduct(model);
+            return Ok(response);
+        }
     }
 }
